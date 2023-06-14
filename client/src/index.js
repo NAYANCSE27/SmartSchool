35afb57e5a08c508842f8726c8276abc5e5b7175
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-06-14 15:37:47 +0600

    added antd package to the project

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ConfigProvider } from "antd";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider
+      theme={{
+        components: {
+          Button: {
+            colorPrimary: "#062925",
+            colorPrimaryHover: "#062925",
+            borderRadius: "0px",
+          },
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
 
