25610727cbbe73adf35fecb45f96f41075cfdfc6
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-06-16 21:48:09 +0600

    added login endpoint in the project

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
 const port = process.env.PORT || 5000;
+
+const userRoute = require("./routes/usersRoute");
+
+app.use("/api/users", userRoute);
 
 app.listen(port, () => console.log(`Listening on port node/express ${port}`));
