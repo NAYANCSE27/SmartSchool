2a4e7da3db18cd5053655bb67ad5c5909c0e4e55
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-06-21 19:58:58 +0600

    added simple navbar and protect the nodemon loading for frontend changing

--- client/src/components/ProtectedPage.js
+++ client/src/components/ProtectedPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { message } from "antd";
+import { Avatar, Badge, message } from "antd";
 import { GetCurrentUser } from "../api/users";
 import { useNavigate } from "react-router-dom";
 
@@ -32,9 +32,25 @@ function ProtectedPage({ children }) {
 
   return (
     user && (
-      <div className="p-5">
-        {user.name}
-        {children}
+      <div>
+        {/* header section */}
+        <div className="flex justify-between items-center bg-primary p-5">
+          <h1 className="text-2xl text-white cursor-pointer">SMART-SCHOOL</h1>
+
+          <div className="bg-white py-2 px-5 rounded flex gap-1 items-center">
+            <i className="ri-shield-user-line"></i>
+            <span className="underline cursor-pointer uppercase">
+              {user.name}
+            </span>
+            <i
+              className="ri-logout-box-r-line ml-7 cursor-pointer"
+              onClick={() => {
+                localStorage.removeItem("token");
+                navigate("/login");
+              }}
+            ></i>
+          </div>
+        </div>
       </div>
     )
   );
